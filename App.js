ce007ff1231f97dbda128068221a70490393c275
nousi <nousidayo@gmail.com>
2020-06-18 18:33:40 +0900

    ダミーデータをFlatListを使って表示
    スクロールすることが可能なため、FlatListに切り替えた

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import ListItem from "./components/ListItem";
 import articles from "./dummies/articles";
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return (
-      <ListItem
-        imageUrl={article.urlToImage}
-        text={article.title}
-        author={article.author}
-        key={index}
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            text={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
-    );
-  });
-  return <View style={styles.container}>{items}</View>;
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
