bbbee85e75407f1aee773f014991534e13b740bc
nousi <nousidayo@gmail.com>
2020-06-18 16:44:40 +0900

    App.jsに記述したstylesheetをコピー
    ニュースリストのstyleをApp.jsに記述していたので、それをコピーして移植した

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,2 +1,34 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection: "column",
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 15,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
+  },
+});
