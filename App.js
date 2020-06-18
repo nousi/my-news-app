86ffb9fb216a000dcb716ba85c04434099dfa504
nousi <nousidayo@gmail.com>
2020-06-18 15:36:13 +0900

    inital commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Text>あああああああ</Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <Text>ここが変更される</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +19,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  leftContainer: {
+    backgroundColor: "blue",
+    width: 100,
+    height: "100%",
+  },
+  rightContainer: {
+    backgroundColor: "red",
+    flex: 1,
+    height: "100%",
   },
 });
