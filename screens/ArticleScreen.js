e18d3729bc07242f1c885d48e38df5939361102d
nousi <nousidayo@gmail.com>
2020-06-20 11:11:56 +0900

    記事画面であるArticleScreen.jsを作成
    ここでは土台だけ記述

---
+++ screens/ArticleScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+
+export default ArticleScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>ここが記事の画面です</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
