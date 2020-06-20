97dc0d32c76b1d11a5f7cdfaa83dee784f432c98
nousi <nousidayo@gmail.com>
2020-06-20 09:59:00 +0900

    App.jsからコピーした不要な記述を削除

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,34 +11,12 @@ const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.m
 const Stack = createStackNavigator();
 
 export default function HomeScreen() {
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            text={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
