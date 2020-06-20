060c13877958e00a63b9231eb0a09a6d9b107615
nousi <nousidayo@gmail.com>
2020-06-20 10:08:45 +0900

    React NavigatorとStackNavigatorをimport

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import Constants from "expo-constants";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createStackNavigator();
