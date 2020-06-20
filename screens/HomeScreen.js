4669cd7017d0e746a880c178b1e7a25a5732ddf8
nousi <nousidayo@gmail.com>
2020-06-20 10:02:17 +0900

    HomeScreenに必要な記述を戻す
    App.jsに不要なだけで、HomeScreenに必要な記述だったため、記述を戻す

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,12 +11,34 @@ const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.m
 const Stack = createStackNavigator();
 
 export default function HomeScreen() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            text={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
