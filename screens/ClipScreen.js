10ec1c80c606b00ef13800cb8bc156b939388cd8
nousi <nousidayo@gmail.com>
2020-06-26 23:34:39 +0900

    Clipの一覧を表示にFlatListを利用

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { StyleSheet, SafeAreaView, FlatList, Text } from "react-native";
+import ListItem from "../components/ListItem";
 
-export default ArticleScreen = () => {
+export default ClipScreen = ({ navigation }) => {
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Clip Screen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            text={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate("Article", { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
