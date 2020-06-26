101dda331ce3915a390cbc135523c7f6a9aa3bb7
nousi <nousidayo@gmail.com>
2020-06-26 19:45:32 +0900

    重複した記述を削除
    createStoreを追加

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-import { combinReducers, combineReducers } from "redux";
+import { createStore, combineReducers } from "redux";
 import userReducer from "./reducers/user";
 import { composeWithDevTools } from "redux-devtools-extension";
 
