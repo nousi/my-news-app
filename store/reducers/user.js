59b6d0391d07c7dc1cde1a1aff3a9ac62286c76b
nousi <nousidayo@gmail.com>
2020-06-26 19:45:47 +0900

    state = initialStateを追記

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -2,7 +2,7 @@ const initialState = {
   clips: [],
 };
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_CLIP":
       return {
