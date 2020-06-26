db78c4b1de9b301a32fb8059e6244b64ce38c6e0
nousi <nousidayo@gmail.com>
2020-06-26 18:54:09 +0900

    Update ArticleScreen.js
    TouchableOpacityをimport
    actionからimport { addClip }

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
 import { useDispatch } from "react-redux";
+import { addClip } from "../store/actions/user";
 
 export default ArticleScreen = ({ route }) => {
   const dispatch = useDispatch();
   const { article } = route.params;
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
