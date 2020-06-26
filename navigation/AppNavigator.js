eb8e33aa8eef0d4e9baa7bee273e4c8f4641dcc7
nousi <nousidayo@gmail.com>
2020-06-26 21:36:30 +0900

    ClipStackを作成

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
+import ClipScreen from "../screens/ClipScreen";
 import ArticleScreen from "../screens/ArticleScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -23,12 +24,24 @@ const HomeStack = () => {
     </Stack.Navigator>
   );
 };
+const ClipStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Clip"
+        component={ClipScreen}
+        options={{ title: "保存した記事" }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Clip" component={ClipStack} />
       </Tab.Navigator>
     </NavigationContainer>
   );
