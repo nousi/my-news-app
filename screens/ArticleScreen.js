3f1afcb5945d13692501680396eaa1ae48bfc3ac
nousi <nousidayo@gmail.com>
2020-06-27 23:28:03 +0900

    isClippedを定義

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addClip } from "../store/actions/user";
 
 export default ArticleScreen = ({ route }) => {
-  const dispatch = useDispatch();
   const { article } = route.params;
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
+  const isClipped = () => {
+    return clips.some((clip) => clip.url === article.url);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity
