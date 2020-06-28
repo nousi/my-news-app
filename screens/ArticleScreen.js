52f93fec35978b3b6bb3ffc1b3aaa02f3998c6f4
nousi <nousidayo@gmail.com>
2020-06-28 17:31:40 +0900

    toggleClip でdispatchの処理分岐
    <ClipButton onPress={toggleClip} enabled={isClipped()} />に短縮

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -3,24 +3,30 @@ import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
 import { useDispatch, useSelector } from "react-redux";
 import { addClip } from "../store/actions/user";
+import { deleteClip } from "../store/actions/user";
+import ClipButton from "../components/ClipButton";
 
 export default ArticleScreen = ({ route }) => {
   const { article } = route.params;
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const { clips } = user;
+
   const isClipped = () => {
     return clips.some((clip) => clip.url === article.url);
   };
+
+  const toggleClip = () => {
+    if (isClipped()) {
+      dispatch(deleteClip({ clip: article }));
+    } else {
+      dispatch(addClip({ clip: article }));
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          dispatch(addClip({ clip: article }));
-        }}
-      >
-        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
-      </TouchableOpacity>
+      <ClipButton onPress={toggleClip} enabled={isClipped()} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
