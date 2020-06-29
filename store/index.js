8dc3dc2ba09ccdfd1c08de6146285626cda5b8f8
nousi <nousidayo@gmail.com>
2020-06-29 15:44:12 +0900

    本体保存するように設定

--- store/index.js
+++ store/index.js
@@ -2,12 +2,21 @@ import { createStore, combineReducers } from "redux";
 import userReducer from "./reducers/user";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { AsyncStorage } from "react-native";
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["user"],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
