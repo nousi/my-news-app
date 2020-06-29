c8e853307a75dc47b9264ab23144f75ccaf9553a
nousi <nousidayo@gmail.com>
2020-06-29 15:21:51 +0900

    インストールしたredux-persistをimport

--- store/index.js
+++ store/index.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers } from "redux";
 import userReducer from "./reducers/user";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
